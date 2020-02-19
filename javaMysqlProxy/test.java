fe9f6ccf8f89aa05b3ddd26de893a59cb6a27ad6
XiaoQQin <974137326@qq.com>
2020-02-19 16:23:48 +0800

    fix some bugs about javaMysqlProxy

--- javaMysqlProxy/test.java
+++ javaMysqlProxy/test.java
@@ -4,12 +4,12 @@ import pool.MysqlProxy;
 
 public class test {
     public static void main(String[] args) {
-
         try {
             GenericObjectPool<MysqlProxy> poolObject = CreateMysqlPool.getInstance().getPoolObject();
             MysqlProxy mysqlProxy = poolObject.borrowObject();
-            String sql = "INSERT INTO test_pool VALUES(?)";
-
+            String sql = "INSERT INTO test_pool (id,value) VALUES(?,?)";
+            String [] values={"daf","fda"};
+            mysqlProxy.executeUpdate(sql,values);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- javaMysqlProxy/utils/ConfigurationManager.java
+++ javaMysqlProxy/utils/ConfigurationManager.java
@@ -3,14 +3,14 @@ package utils;
 import org.apache.commons.configuration2.FileBasedConfiguration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
-import org.apache.commons.configuration2.builder.fluent.FileBasedBuilderParameters;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 
-public class ConfigurationManager {
-    public static  FileBasedConfiguration config;
 
-    private ConfigurationManager(){
+public class ConfigurationManager {
+    public static FileBasedConfiguration config;
+
+    static {
         try {
             Parameters params =new Parameters();
             FileBasedConfigurationBuilder<FileBasedConfiguration> builder=
@@ -22,7 +22,9 @@ public class ConfigurationManager {
         }
     }
 
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        System.out.println(ConfigurationManager.config.getString(Constant.JDBC_URL));
     }
 }
